d299ba4a0cd416c91be82dc12f4f690f34e928fa
MooseInTheSack <tyl3rkirkpatrick@yahoo.com>
2020-01-16 20:04:53 -0600

    Updates

---
+++ precache-manifest.249192ba5fc983c36ff619575ea9e7a2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a6db2827b9c2d9bb68815108bcba766",
+    "url": "/ryan-react-app/index.html"
+  },
+  {
+    "revision": "11d4c1ecf1e036048649",
+    "url": "/ryan-react-app/static/css/2.1cc9201e.chunk.css"
+  },
+  {
+    "revision": "87d5a707c0a870fb7efe",
+    "url": "/ryan-react-app/static/css/main.41aa44ff.chunk.css"
+  },
+  {
+    "revision": "11d4c1ecf1e036048649",
+    "url": "/ryan-react-app/static/js/2.7304ef3a.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/ryan-react-app/static/js/2.7304ef3a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "87d5a707c0a870fb7efe",
+    "url": "/ryan-react-app/static/js/main.0e51677e.chunk.js"
+  },
+  {
+    "revision": "99c2aad5fd1e11b1261d",
+    "url": "/ryan-react-app/static/js/runtime-main.4a3b657a.js"
+  },
+  {
+    "revision": "748abc16ecb3926d57fa043658a8a820",
+    "url": "/ryan-react-app/static/media/Avengers_poster.748abc16.jpg"
+  },
+  {
+    "revision": "7f4a8e699352133a26d34bf889bde32a",
+    "url": "/ryan-react-app/static/media/Ben_4.7f4a8e69.jpg"
+  },
+  {
+    "revision": "d5206331be94dd4a4f120e9a9f177878",
+    "url": "/ryan-react-app/static/media/Buddy_the_ant.d5206331.jpg"
+  },
+  {
+    "revision": "6d0094b28832a22319997f774397abf6",
+    "url": "/ryan-react-app/static/media/ben_3.6d0094b2.jpg"
+  },
+  {
+    "revision": "becfcb52f1f78b5f910de1f8d98dabce",
+    "url": "/ryan-react-app/static/media/blackmane.becfcb52.jpg"
+  }
+]);
