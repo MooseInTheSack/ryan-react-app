32307b8080060c8cca1ecc64bae9e5c397554963
MooseInTheSack <tyl3rkirkpatrick@yahoo.com>
2020-03-29 13:38:19 -0500

    changed about message

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,8 @@ function About() {
   return (
     <div className="About">
         <h2>About</h2>
-        <p>About Ryan: he sucks...</p>
+        <p>About me:</p>
+        <p>I am an aspiring young artist aiming to share my creativity with the world. I specialize in illustrations, figure drawing, inking, and digital work. I also have experience in oil painting, 3D modeling, and graphic design.</p>
       
     </div>
   );
