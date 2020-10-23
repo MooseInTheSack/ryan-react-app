9ae67e4936c743fcf13624a45ab4f514b1bbc2f5
MooseInTheSack <tyl3rkirkpatrick@yahoo.com>
2020-10-23 13:39:27 -0500

    Updates

---
+++ precache-manifest.072bd22cdb256673837185338245d374.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1373677dea0b41b2b06e52b538a7042f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2f534e38976735193a5",
+    "url": "/static/css/2.40a775a0.chunk.css"
+  },
+  {
+    "revision": "3222ed4455f0e611e99b",
+    "url": "/static/css/main.0e8e926a.chunk.css"
+  },
+  {
+    "revision": "a2f534e38976735193a5",
+    "url": "/static/js/2.233afc5d.chunk.js"
+  },
+  {
+    "revision": "2d44b0d619a12c1417728a085056f2e7",
+    "url": "/static/js/2.233afc5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3222ed4455f0e611e99b",
+    "url": "/static/js/main.901c2ba6.chunk.js"
+  },
+  {
+    "revision": "1bf5d5542487f3e7386f",
+    "url": "/static/js/runtime-main.b63d8feb.js"
+  },
+  {
+    "revision": "a74ed2de8af4a3580a8d84387b13412c",
+    "url": "/static/media/001.a74ed2de.jpg"
+  },
+  {
+    "revision": "438fdfb7b49f3738e755f4bc394322b7",
+    "url": "/static/media/002.438fdfb7.jpg"
+  },
+  {
+    "revision": "185ca7888aa97ff2c98fc944a1ae29bb",
+    "url": "/static/media/003.185ca788.jpg"
+  },
+  {
+    "revision": "cc8fe18171faa4e391d9ea1cb7762e5d",
+    "url": "/static/media/004.cc8fe181.jpg"
+  },
+  {
+    "revision": "d8cd89d9cede0ed247845a534bd45969",
+    "url": "/static/media/005.d8cd89d9.jpg"
+  },
+  {
+    "revision": "f8944006f400a11e07b527cce220f262",
+    "url": "/static/media/006.f8944006.jpg"
+  },
+  {
+    "revision": "6d440d4fde4b6b7f5044dc35d302c427",
+    "url": "/static/media/007.6d440d4f.jpg"
+  },
+  {
+    "revision": "0f26bb144d895ff9af844ab6587bc311",
+    "url": "/static/media/009.0f26bb14.jpg"
+  },
+  {
+    "revision": "528f18b816a75a8134480cc25195f8de",
+    "url": "/static/media/010.528f18b8.jpg"
+  },
+  {
+    "revision": "93c871dd668eb08363fadbc56b4a10fd",
+    "url": "/static/media/011.93c871dd.jpg"
+  },
+  {
+    "revision": "0f2481124662f8879c973fa5b346e230",
+    "url": "/static/media/012.0f248112.jpg"
+  },
+  {
+    "revision": "9254453ee25a55394b778eb4d269e71c",
+    "url": "/static/media/013.9254453e.jpg"
+  },
+  {
+    "revision": "509adcd5e2af6dbcec9f7181f6fc560e",
+    "url": "/static/media/014.509adcd5.jpg"
+  },
+  {
+    "revision": "3b87c9e5a300220d616d7e4d9fbafd08",
+    "url": "/static/media/015.3b87c9e5.jpg"
+  },
+  {
+    "revision": "e7b8fec7c15ec91ed1bfaebac096fa17",
+    "url": "/static/media/016.e7b8fec7.jpg"
+  },
+  {
+    "revision": "8c9b73134cf05c7010a4ad70653f76a1",
+    "url": "/static/media/017.8c9b7313.jpg"
+  },
+  {
+    "revision": "748abc16ecb3926d57fa043658a8a820",
+    "url": "/static/media/Avengers_poster.748abc16.jpg"
+  },
+  {
+    "revision": "7f4a8e699352133a26d34bf889bde32a",
+    "url": "/static/media/Ben_4.7f4a8e69.jpg"
+  },
+  {
+    "revision": "d5206331be94dd4a4f120e9a9f177878",
+    "url": "/static/media/Buddy_the_ant.d5206331.jpg"
+  },
+  {
+    "revision": "969bfa3a5f0e67e2330aa4613adcce62",
+    "url": "/static/media/Dr_Ferald.969bfa3a.jpg"
+  },
+  {
+    "revision": "bf85b48943537c9d1236f807f6ba6943",
+    "url": "/static/media/Ho_ho_whore.bf85b489.jpg"
+  },
+  {
+    "revision": "d09cfe35df8b6362dc781b1b6e6b8905",
+    "url": "/static/media/IMG_1552.d09cfe35.jpeg"
+  },
+  {
+    "revision": "3f5a9c24c6741e6423174b3c0a57a35b",
+    "url": "/static/media/IMG_1575.3f5a9c24.jpeg"
+  },
+  {
+    "revision": "9ea7f46daf8dd1088740d5b6b2a5fbe8",
+    "url": "/static/media/IMG_1762.9ea7f46d.jpeg"
+  },
+  {
+    "revision": "54f0dc30f7d46a7ce298f3f05531a2c8",
+    "url": "/static/media/IMG_1768.54f0dc30.jpeg"
+  },
+  {
+    "revision": "a651c41ec31252703f72f48cdf52967d",
+    "url": "/static/media/IMG_1775.a651c41e.jpeg"
+  },
+  {
+    "revision": "c38075ab56dcc1d4ce6651731eb3ef04",
+    "url": "/static/media/IMG_1786.c38075ab.jpeg"
+  },
+  {
+    "revision": "86cab7b92fabe5ab464471e2a2b37f40",
+    "url": "/static/media/IMG_1804.86cab7b9.jpeg"
+  },
+  {
+    "revision": "299913a083ad86bbbbbb97fde1d1512d",
+    "url": "/static/media/IMG_1807.299913a0.jpeg"
+  },
+  {
+    "revision": "5570efc6c372143df47508ecfbfc701e",
+    "url": "/static/media/IMG_1808.5570efc6.jpeg"
+  },
+  {
+    "revision": "44fc8534293b76395ae504794949b824",
+    "url": "/static/media/IMG_1809.44fc8534.jpeg"
+  },
+  {
+    "revision": "7440fd3cfab7eb2755e24b4fd1b67d5a",
+    "url": "/static/media/Rumple_and_Horace.7440fd3c.jpg"
+  },
+  {
+    "revision": "cdfed17efabda6e60fc70c394dc14400",
+    "url": "/static/media/Way_out_West_image.cdfed17e.jpg"
+  },
+  {
+    "revision": "6d0094b28832a22319997f774397abf6",
+    "url": "/static/media/ben_3.6d0094b2.jpg"
+  },
+  {
+    "revision": "becfcb52f1f78b5f910de1f8d98dabce",
+    "url": "/static/media/blackmane.becfcb52.jpg"
+  },
+  {
+    "revision": "547376e03e433c9055b20da4d6cd4206",
+    "url": "/static/media/buddy_2.547376e0.jpg"
+  },
+  {
+    "revision": "214c0f58c123482e3794294394e9415b",
+    "url": "/static/media/buddy_3.214c0f58.jpg"
+  },
+  {
+    "revision": "18daaa4f6a42eead6dea9ec80c55aa8a",
+    "url": "/static/media/fisherman.18daaa4f.jpg"
+  },
+  {
+    "revision": "18a85d3eb703342d7238ae7e5e32eea5",
+    "url": "/static/media/iron_man_vs_thanos.18a85d3e.jpg"
+  },
+  {
+    "revision": "12825b956e53d8117dc6b5ee1786e316",
+    "url": "/static/media/new_buddy.12825b95.jpg"
+  },
+  {
+    "revision": "6b73b329ac7adeb1c725be3684c855cd",
+    "url": "/static/media/rodrigo_2099.6b73b329.jpg"
+  },
+  {
+    "revision": "590c73eb664669be59f9d116eac78985",
+    "url": "/static/media/scarecrow_boy.590c73eb.jpg"
+  },
+  {
+    "revision": "4c39fdcd26ad4a1065fc285c340c42c5",
+    "url": "/static/media/scorpion.4c39fdcd.jpg"
+  },
+  {
+    "revision": "cb2f2b00b72508cbadd28892793ee2c5",
+    "url": "/static/media/self_p.cb2f2b00.jpg"
+  },
+  {
+    "revision": "e7ab18416f94bc5166c09a320a438591",
+    "url": "/static/media/self_p_small.e7ab1841.jpg"
+  },
+  {
+    "revision": "969ee17e01087d7614301122b851f699",
+    "url": "/static/media/self_portrait_copy.969ee17e.jpg"
+  },
+  {
+    "revision": "1f120fabbcc45387a102b55793ad35d1",
+    "url": "/static/media/sourcerer.1f120fab.jpg"
+  },
+  {
+    "revision": "6cc6720c9f5054601a4e71436164611f",
+    "url": "/static/media/spiderverse.6cc6720c.jpg"
+  },
+  {
+    "revision": "4fea84ce79248496dd05cabc14766f3f",
+    "url": "/static/media/treasure_hunters.4fea84ce.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6c748ac4db12317c222ef37652b72e1a.js"
+  "/precache-manifest.072bd22cdb256673837185338245d374.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
